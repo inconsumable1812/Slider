120aa91630e71e783fe70e235776289c6f2caa84
inconsumable1812 <64452918+inconsumable1812@users.noreply.github.com>
2021-10-13 23:12:27 +0800

    Разобрался с jsdom и добавил coverage

--- src/components/views/handle/Handle.test.ts
+++ src/components/views/handle/Handle.test.ts
@@ -1,14 +1,10 @@
 import Handle from './Handle'
 
-const jsdom = require('jsdom')
-const { JSDOM } = jsdom
-
-global.document = new JSDOM('').window.document
-
 const handel = new Handle()
 
 describe('TEST', () => {
   test('test', () => {
-    expect(handel).toBeDefined()
+    const element = document.createElement('div')
+    expect(element).toBeDefined()
   })
 })
