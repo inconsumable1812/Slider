cc2e7e61acabf34c70cafcb0eea58fa9a7ca8253
inconsumable1812 <64452918+inconsumable1812@users.noreply.github.com>
2021-10-20 17:39:43 +0800

    Подправил отображение на мобильных

--- src/demo/demo.ts
+++ src/demo/demo.ts
@@ -1,21 +1,29 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import '../components/slider'
-import { create } from '../components/slider'
-import '../components/JQplugin'
-
 import $ from 'jquery'
+import '../components/slider'
+import '../components/JQplugin'
+import { sliderOptions } from 'components/type'
 
 const slider = $('#app').JQSlider()
 slider.JQSlider('addControlPanel')
 
-const sliderSecondConfig = {
+const sliderSecondConfig: Partial<sliderOptions> = {
   minValue: 500,
   maxValue: 1000
 }
 
-const sliderThirdConfig = {
-  minValue: 500,
-  maxValue: 1000
+const sliderThirdConfig: Partial<sliderOptions> = {
+  minValue: -500,
+  maxValue: -400,
+  valueStart: -450,
+  range: true,
+  showProgress: true,
+  scalePointCount: 5
+}
+
+const sliderFourthConfig: Partial<sliderOptions> = {
+  isVertical: true,
+  showProgress: true
 }
 
 const sliderSecond = $('#app_2').JQSlider()
@@ -24,3 +32,6 @@ sliderSecond.JQSlider('setOptions', { minValue: 5 })
 
 const sliderThird = $('#app_3').JQSlider(sliderThirdConfig)
 const sliderThirdPanel = sliderThird.JQSlider('addControlPanel')
+
+const sliderFourth = $('#app_3').JQSlider(sliderFourthConfig)
+const sliderFourthPanel = sliderFourth.JQSlider('addControlPanel')
